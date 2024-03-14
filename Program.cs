4674bc6a0bd25287d29345434be2b967a7e3a21b
danellopes <daniel.nunes.meira@outlook.com>
2024-03-14 08:24:36 -0300

    Prototype Design Pattern using an Explicit Deep Copy Interface

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace C_ExplicitDeepCopyInterface;
+
+class Program
+{
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+
+    public class Person : IPrototype<Person>
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            if (names == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(names));
+            }
+            if (address == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(address));
+            }
+
+            Names = names;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"Names: {string.Join(" ", Names)}, Address: {Address}";
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(Names, Address.DeepCopy());
+        }
+    }
+
+    public class Address : IPrototype<Address>
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            if (streetName == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(streetName));
+            }
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"StreetName: {StreetName}, HouseNumber: {HouseNumber}";
+        }
+
+        public Address DeepCopy()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+        var jane = john.DeepCopy();
+        jane.Names = new[] { "Jane", "Smith" };
+        jane.Address.HouseNumber = 321;
+
+        System.Console.WriteLine(john);
+        System.Console.WriteLine(jane);
+    }
+}
